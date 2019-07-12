82947683cbf24e7148f6660bdb29da27e96885f5
user <user@localhost.localdomain>
2019-07-12 21:45:39 +0100

    12
    Co-Authored-By: Mohammad Amin Ghaemi <Faral-Ghaemi@users.noreply.github.com>

--- ClassKu/settings.py
+++ ClassKu/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.gis.db',
 ]
 
 ROOT_URLCONF = 'ClassKu.urls'
